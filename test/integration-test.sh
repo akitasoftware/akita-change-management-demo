40b5f9ca76beea849734ca9ced75626729f857d6
Chris Corcoran <chris@akitasoftware.com>
2020-10-30 14:42:15 -0700

    Adding loggin

--- test/integration-test.sh
+++ test/integration-test.sh
@@ -4,7 +4,7 @@
 json_status_code=(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8080/users/json)
 
 if [ $json_status_code != "%200" ]; then
-  echo "JSON endpoint returned error code"
+  echo "JSON endpoint returned error code: $json_status_code"
   exit 1
 fi
 
@@ -12,6 +12,6 @@ fi
 yaml_status_code=(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8080/users/json)
 
 if [ yaml_status_code != "%200" ]; then
-  echo "JSON endpoint returned error code"
+  echo "JSON endpoint returned error code: $yaml_status_code"
   exit 1
 fi
