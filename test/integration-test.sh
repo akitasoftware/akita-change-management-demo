5950855902cadbdc5170dc3d9db0b46ce9e56672
Chris Corcoran <chris@akitasoftware.com>
2020-10-30 14:40:17 -0700

    Adding integration tests

---
+++ test/integration-test.sh
@@ -0,0 +1,17 @@
+#/bin/sh
+
+# Test the JSON endpint
+json_status_code=(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8080/users/json)
+
+if [ $json_status_code != "%200" ]; then
+  echo "JSON endpoint returned error code"
+  exit 1
+fi
+
+# Test the YAML endpoint
+yaml_status_code=(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8080/users/json)
+
+if [ yaml_status_code != "%200" ]; then
+  echo "JSON endpoint returned error code"
+  exit 1
+fi
