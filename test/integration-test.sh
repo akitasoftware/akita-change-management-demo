8cb37c296ce40e24d7671c3daf79871dd78110e8
Chris Corcoran <chris@akitasoftware.com>
2020-10-30 15:03:51 -0700

    Adding yaml endpoint

--- test/integration-test.sh
+++ test/integration-test.sh
@@ -8,4 +8,12 @@ if [ $json_status_code != 200 ]; then
   exit 1
 fi
 
+# Test the YAML endpoint
+yaml_status_code=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8080/users/yaml)
+
+if [ $yaml_status_code != 200 ]; then
+  echo "JSON endpoint returned error code: $yaml_status_code"
+  exit 1
+fi
+
 echo "All test passed successfully!"
