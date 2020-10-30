8d3db04dbb32bb185c3006e19c5ce4f0a0b49dd6
Chris Corcoran <christopher.f.corcoran@gmail.com>
2020-10-30 14:49:54 -0700

    Adding CircleCI
    Adding CircleCI configuration and integration tests for the demo

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/akitasoftware/blog6
 
 require gopkg.in/yaml.v2 v2.3.0
+
+go 1.13
---
+++ test/integration-test.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+
+# Test the JSON endpint
+json_status_code=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8080/users/json)
+
+if [ $json_status_code != 200 ]; then
+  echo "JSON endpoint returned error code: $json_status_code"
+  exit 1
+fi
+
+# Test the YAML endpoint
+yaml_status_code=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8080/users/yaml)
+
+if [ $yaml_status_code != 200 ]; then
+  echo "JSON endpoint returned error code: $yaml_status_code"
+  exit 1
+fi
+
+echo "All test passed successfully!"
